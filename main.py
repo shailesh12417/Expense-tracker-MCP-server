86c5142b8cd696c8561f307bba9d3cf9908ce8f2
Shailesh Sahu <salusahu374@gmail.com>
2026-04-01 00:16:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastmcp import FastMCP
 import os
 import aiosqlite
 import json
-
+import tempfile
 # ------------------ PATH SETUP ------------------
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "expenses.db")
+TEMP_DIR = tempfile.gettempdir()
+DB_PATH = os.path.join(TEMP_DIR, "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
 print(f"Database path: {DB_PATH}")
